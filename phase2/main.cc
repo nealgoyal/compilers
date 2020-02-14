c6a131078fd993a96c19d02b1518ad57072d93ea
cameronmorin <morincam03@gmail.com>
2020-02-13 16:44:28 -0800

    Change exit(1) to exit(0)

--- phase2/main.cc
+++ phase2/main.cc
@@ -1,11 +1,12 @@
 #include "heading.h";
+#include <stdlib.h>;
 
 int yyparse();
 
 int main(int argc, char **argv) {
   if ((argc > 1) && (freopen(argv[1], "r", stdin) == NULL)) {
     cerr << argv[0] << ": File " << argv[1] << " cannot be opened.\n";
-    exit( 1 );
+    exit(0);
   }
   
   yyparse();
