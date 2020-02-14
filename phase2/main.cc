ec3ff0d038503da0bea21cab9b36aa3748842210
cameronmorin <morincam03@gmail.com>
2020-02-13 16:15:13 -0800

    Initialize main and header files

---
+++ phase2/main.cc
@@ -0,0 +1,14 @@
+#include "heading.h";
+
+int yyparse();
+
+int main(int argc, char **argv) {
+  if ((argc > 1) && (freopen(argv[1], "r", stdin) == NULL)) {
+    cerr << argv[0] << ": File " << argv[1] << " cannot be opened.\n";
+    exit( 1 );
+  }
+  
+  yyparse();
+
+  return 0;
+}
